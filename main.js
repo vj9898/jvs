8614367c30504fb958a5d356128315bc338d9393
vj9898 <81862570+vj9898@users.noreply.github.com>
2021-05-07 12:13:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var firstname="ved";
+console.log(firstname);
+
+var lastname="joshi";
+console.log(lastname);
+
+var fullname=firstname.concat(lastname);
+console.log(fullname);
+
+var square_root = Math.pow(16,18);
+console.log(square_root);
+
+var no=1;
+
+var no2=8;
+
+var add = no+no2;
+console.log(add);
+
+var subs = no2-no;
+console.log(subs);
+
+var multi = no*no2;
+console.log(multi);
+
+var div = no2/no;
+console.log(div);
